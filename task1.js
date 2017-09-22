1d9e1ba881efae7469a4e90f4895e925c884f087
Jennifer <yashijennifer@gmail.com>
2017-09-22 15:59:04 +0100

    modified game

--- task1.js
+++ task1.js
@@ -1,15 +1,52 @@
-var counter = 0;
-
-function person(id, name, score, profile) {
-  this.name = id,
+var counter = 1;
+var persons=[];
+function Person(id, name, score, profile) {
+  this.id = id,
   this.name = name,
   this.score = score,
-  this.profile = "http://media.istockphoto.com/photos/male-silhouette-as-avatar-profile-picture-picture-id519078727?k=6&m=519078727&s=612x612&w=0&h=ptX9HZCoDQh3i5ZjC_j3HltiaeeJK-HfHrCa35Chx3M="
+  this.profile = "";
 }
 
 function getName(){
-    let name= document.getElementById('textbox_id').value;
+
+    let name= document.getElementById('txtName').value;
     if(!name) {
-      prompt ('Please enter your name');
+      prompt ('Error! Please enter your name');
+    }
+    else {
+      return name;
     }
 }
+
+function setScore() {
+  var score = Math.random(1) * 10;
+    score=score.toFixed(2); //to round off to 2 decimal places
+}
+
+function sortByScore() {
+  person.sort(function(a,b){
+    return a.score-b.score;
+  });
+}
+
+function sortById() {
+  person.sort(function(a,b){
+    return a.id - b.id;
+  });
+}
+
+function addPlayer() {
+  let id=counter;
+  let name=getName();
+  let score=0;
+  let profile="http://media.istockphoto.com/photos/male-silhouette-as-avatar-profile-picture-picture-id519078727?k=6&m=519078727&s=612x612&w=0&h=ptX9HZCoDQh3i5ZjC_j3HltiaeeJK-HfHrCa35Chx3M=";
+  var person=new Person(id,name,score,profile);
+  persons.push(person);
+    counter++;
+  }
+
+  //to keep track of the infor
+  persons.map(function(Person){
+    Person.setScore;
+
+  })
